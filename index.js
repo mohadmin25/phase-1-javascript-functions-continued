686c587ba61805e84004f012c399a8c07b08eddd
mohadmin25 <abuhamar008@gmail.com>
2022-08-18 20:34:42 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function saturdayFun( roller = "roller-skate"){
+    return `This Saturday, I want to ${roller}!`
+ }
+
+function mondayWork( office = 'go to the office'){
+    return `This Monday, I will ${office}.`
+ }
+ let wrapAdjective = function (initial = "*"){
+    return function (text = "mohadmin"){
+        return `You are ${initial}${text}${initial}!`
+    }
+}
